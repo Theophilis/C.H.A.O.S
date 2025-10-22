d687cace065f5d597ee4e04d1c30d803082a116c
odle-eleton <128094542+odle-eleton@users.noreply.github.com>
2025-10-22 11:02:42 -0700

    flow synth

--- C.H.A.O.S/meca/meca.py
+++ C.H.A.O.S/meca/meca.py
@@ -2,9 +2,10 @@ import pygame
 import pygame.camera
 import pygame.surfarray as surfarray
 import pygame.font
-import cv2
 import numpy as np
 import pickle
+import time
+from datetime import datetime
 
 
 
@@ -179,6 +180,11 @@ score = 1
 score_h = 0
 sign_bank = {}
 phrase_past = phrase[::]
+tts = [0, 0]
+times = []
+tts_0 = time.time()
+tts_1 = time.time()
+stenograph = []
 
 signame = "Theophilis"
 
@@ -190,9 +196,12 @@ try:
 
     print("loading" + signame)
 
+    stenograph = sign_bank['steno']
+
 except:
     print('new user')
     sign_bank = {}
+
 
 
 ram = {}
@@ -217,11 +226,11 @@ rule = np.array(rule)
 
 print(rule)
 
-l = 480
+l = 592
 h = l
 lh = l * h
 pos_x = int(screen_width / 2) - int(l / 2)
-pos_y = int(screen_height / 3.5) - int(h / 2)
+pos_y = int(screen_height / 3) - int(h / 2)
 pos_z = 0
 
 if dim == 1:
@@ -236,8 +245,184 @@ if dim == 2:
     water = np.zeros((h, l), dtype=int)
 
 
-###mask###
-mask_color = (255, 255, 255)
+###clock###
+time_d = time.time()
+time_b = time.time()
+print(time_d)
+print(time_b)
+
+
+
+
+##sythn##
+
+
+sample_rate = 44100
+duration = 1
+frequency = 440 * (2 ** 1 / 12) ** digibet[code_0]
+max_amplitude = 2 ** 15 - 1
+previous_note = []
+
+
+t = np.linspace(0, duration, int(sample_rate * duration), False)
+mono_wave = max_amplitude * np.sin(frequency * t * 2 * np.pi)
+mono_samples = mono_wave.astype(np.int16)
+
+pygame.mixer.pre_init(sample_rate, -16, channels=1)
+
+stereo_samples = np.repeat(mono_samples.reshape(-1, 1), 2, axis=1)
+stereo_sound = pygame.sndarray.make_sound(stereo_samples)
+
+
+start = 55
+key_sin = []
+key_square = []
+key_saw = []
+key_triangle = []
+
+key_track = {}
+track_shift = 0
+
+waffle_house = [2, 2, 1, 2, 2, 2, 1]
+pentatonic = [0, 2, 2, 3, 2, 3, 2, 2]
+major = [0, 7, 5, 4, 3, 5, 7, 5]
+
+scale = pentatonic
+octave = 12
+
+for x in range(128):
+
+    ###sin###
+    frequency = start * 2**(x/12)
+    # print(frequency)
+
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    sin_wave = max_amplitude * np.sin(frequency * t * 2 * np.pi)
+    mono_samples = sin_wave.astype(np.int16)
+
+    pygame.mixer.pre_init(sample_rate, -16, channels=1)
+
+    stereo_samples = np.repeat(mono_samples.reshape(-1, 1), 2, axis=1)
+    stereo_sound = pygame.sndarray.make_sound(stereo_samples)
+    key_sin.append(stereo_sound)
+
+
+    ###square###
+    frequency = start * 2**(x/12)
+    # print(frequency)
+
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    square_wave = max_amplitude * np.sign(np.sin(frequency * t * 2 * np.pi))
+    mono_samples = square_wave.astype(np.int16)
+
+    pygame.mixer.pre_init(sample_rate, -16, channels=1)
+
+    stereo_samples = np.repeat(mono_samples.reshape(-1, 1), 2, axis=1)
+    stereo_sound = pygame.sndarray.make_sound(stereo_samples)
+
+    key_square.append(stereo_sound)
+
+    ###saw###
+    frequency = start * 2**(x/12)
+    # print(frequency)
+
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    saw_wave = max_amplitude * (t * frequency % 1 * 2 - 1)
+    mono_samples = saw_wave.astype(np.int16)
+
+    pygame.mixer.pre_init(sample_rate, -16, channels=1)
+
+    stereo_samples = np.repeat(mono_samples.reshape(-1, 1), 2, axis=1)
+    stereo_sound = pygame.sndarray.make_sound(stereo_samples)
+    key_saw.append(stereo_sound)
+
+    ###triangle###
+    frequency = start * 2**(x/12)
+    # print(frequency)
+
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    triangle_wave = max_amplitude * (2 * np.abs(saw_wave / max_amplitude) - 1)
+    mono_samples = triangle_wave.astype(np.int16)
+
+    pygame.mixer.pre_init(sample_rate, -16, channels=1)
+
+    stereo_samples = np.repeat(mono_samples.reshape(-1, 1), 2, axis=1)
+    stereo_sound = pygame.sndarray.make_sound(stereo_samples)
+
+    key_triangle.append(stereo_sound)
+
+
+#drums
+
+def generate_kick(duration_s, sample_rate=44100, max_amplitude=32767):
+
+    t = np.linspace(0, duration_s, int(sample_rate*duration_s), endpoint=False)
+
+    decaying_freq = 200 * np.exp(-t * 20) + 50
+
+    phase = np.cumsum(decaying_freq/sample_rate) * 2 * np.pi
+    kick_wave = max_amplitude * np.sin(phase)
+
+    envelope = np.exp(-t * 8)
+    kick_wave *= envelope
+
+    return kick_wave.astype(np.int16)
+
+def generate_snare(duration_s, sample_rate = 44100, max_amplitude = 32767):
+
+    t = np.linspace(0, duration_s, int(sample_rate * duration_s), endpoint=False)
+
+    body_freq = 200
+    body_envelope = np.exp(-t * 20)
+    body_wave = np.sin(2*np.pi*body_freq*t) * body_envelope
+
+    noise = np.random.uniform(-1, 1, len(t))
+
+    noise_envelope = np.exp(-t * 50)
+    noise_wave = noise * noise_envelope
+
+    snare_wave = (0.5 * body_wave + 0.5 * noise_wave)
+
+    final_envelope = np.exp(-t * 10)
+
+    snare_wave *= final_envelope
+
+    snare_wave *= max_amplitude
+
+    return snare_wave.astype(np.int16)
+
+def generate_hihat(duration_s, sample_rate=44100, max_amplitude=32767):
+
+    t = np.linspace(0, duration_s, int(sample_rate * duration_s), endpoint=False)
+
+    white_noise = np.random.uniform(-1, 1, len(t))
+
+    envelope = np.exp(-t * 40)
+
+    hihat_wave = white_noise * envelope
+    hihat_wave *= max_amplitude
+
+    return hihat_wave.astype(np.int16)
+
+
+
+
+tempo = 0
+
+kick_sample = generate_kick(0.3)
+snare_sample = generate_snare(0.25)
+hihat_sample = generate_hihat(0.1)
+
+
+kick_stereo = np.repeat(kick_sample.reshape(-1, 1), 2, axis=1)
+snare_stereo = np.repeat(snare_sample.reshape(-1, 1), 2, axis=1)
+hihat_stereo = np.repeat(hihat_sample.reshape(-1, 1), 2, axis=1)
+
+
+kick_sound = pygame.sndarray.make_sound(kick_stereo)
+snare_sound = pygame.sndarray.make_sound(snare_stereo)
+hihat_sound = pygame.sndarray.make_sound(hihat_stereo)
+
 
 running = True
 while running:
@@ -302,6 +487,16 @@ while running:
         image_flow = color_array[flow]
         image_array[pos_x:pos_x+l, pos_y:pos_y+h] = image_flow
 
+        flatten = water.flatten()
+        scaled = (flatten * 2 - 1) * max_amplitude
+        sample = scaled.astype(np.int16)
+        stereo = sample.reshape(-1, 1)
+        stack = np.column_stack((stereo, stereo))
+        flow_sound = pygame.sndarray.make_sound(stack)
+
+        flow_sound.play()
+
+
 
     image = pygame.surfarray.make_surface(image_array)
     screen.blit(image, (0, 0))
@@ -312,14 +507,13 @@ while running:
     y_s = 32
     x_g = 4
     y_g = 0
-    x_pos = 128
+    x_pos = 32
     y_pos = 640
-    palm_x = x_pos + x_s*3
-    palm_y = y_pos + y_s*3
-
+    palm_x = x_pos + x_s*6
+    palm_y = y_pos + y_s*9
 
     hand = [0, 0, 0, 0, 0]
-    tips = [48, 16, 0, 24, 0]
+    tips = [64, 16, 0, 24, 0]
 
     for x in range(5):
 
@@ -327,8 +521,8 @@ while running:
 
 
         if x == 4:
-            x_pos += x_s*2
-            y_pos += y_s*6
+            x_pos += x_s*1
+            y_pos += y_s*5
 
 
         roi = (x_pos + x*x_s, y_pos + tips[x], x_pos +x_s + x*x_s, y_pos + y_s + tips[x])
@@ -380,6 +574,9 @@ while running:
         tip_rect = pygame.Rect(roi[0], roi[1], x_s, y_s)
         pygame.draw.rect(screen, value_color[0 + hand[x]*7], tip_rect)
 
+        pygame.draw.line(screen, value_color[0 + hand[x] * 7], (palm_x, palm_y), (roi[0], roi[1]))
+
+
     hand_value = hand[0]*16 + hand[1]*8 + hand[2]*4 + hand[3]*2 + hand[4]*1
 
     lesson_t = main_font.render(str(hand_value), True, value_color[7])
@@ -393,63 +590,35 @@ while running:
 
     hands[0] = letter
 
-    # if phrase != '':
-    #     if phrase != phrase_past:
-    #
-    #         if phrase not in sign_bank:
-    #             sign_bank[phrase] = score
-    #         else:
-    #             score = sign_bank[phrase]
-    #             sign_bank[phrase] = score
-    #
-    #
-    #         print(phrase, sign_bank[phrase])
-    #
-    #         phrase_past = phrase[::]
-    #         score = 1
-    #
-    #         filename = 'sign_bank/' + signame
-    #         outfile = open(filename, 'wb')
-    #         pickle.dump(sign_bank, outfile)
-    #         outfile.close
-    #
-    #
-    #     elif letter == phrase[phrase_pos]:
-    #         phrase_pos += 1
-    #
-    #         if phrase_pos == len(phrase):
-    #
-    #             score += 1
-    #             phrase_pos = 0
-    #             print(sign_bank[phrase])
-
     ####left hand####
     x_s = 32
     y_s = 32
     x_g = 4
     y_g = 0
-    x_pos = 800
+    x_pos = 880
     y_pos = 640
-    palm_x = x_pos + x_s*3
-    palm_y = y_pos + y_s*3
+    palm_x = x_pos + x_s*6
+    palm_y = y_pos + y_s*9
 
 
     hand = [0, 0, 0, 0, 0]
-    tips = [0, 24, 0, 16, 48]
+    tips = [0, 24, 0, 16, 64]
 
     for x in range(5):
 
         x_pos += x_s + x_g
 
         if x == 0:
-            x_pos -= x_s*2
-            y_pos += y_s*6
+            x_pos -= x_s*1
+            y_pos += y_s*5
 
         elif x == 1:
             x_pos += x_s*2
             y_pos -= y_s*6
 
         roi = (x_pos + x * x_s, y_pos + tips[x], x_pos + x_s + x * x_s, y_pos + y_s + tips[x])
+
+        roi_palm = (x_pos + x*x_s, y_pos + y_s*4)
 
         mse, change, roi1, roi2 = detect_change_in_roi_mse(array_past, image_array, roi, thresholds[x+5])
 
@@ -492,6 +661,8 @@ while running:
         t_line = pygame.Rect(roi[0], roi[1], x_s, y_s)
         pygame.draw.rect(screen, value_color[0 + hand[x] * 7], t_line)
 
+        pygame.draw.line(screen, value_color[0 + hand[x] * 7], (palm_x, palm_y), (roi[0], roi[1]))
+
     rethresh = 0
     hand_value = hand[0] * 1 + hand[1] * 2 + hand[2] * 4 + hand[3] * 8 + hand[4] * 16
 
@@ -501,10 +672,34 @@ while running:
     lesson_t = main_font.render(str(digibetu[hand_value]), True, value_color[7])
     screen.blit(lesson_t, (screen_width / 2 + screen_width/4, screen_height / 2 + 96))
 
-
     lesson_t = main_font.render(str(rv), True, value_color[7])
     screen.blit(lesson_t, (screen_width / 2, screen_height / 32))
 
+    lesson_t = main_font.render(str(tts_0), True, value_color[7])
+    screen.blit(lesson_t, (screen_width / 2, screen_height / 4))
+
+
+    ###stats###
+    times_limit = 10
+    for x in range(len(times)):
+        if x > times_limit:
+            break
+        lesson_t = small_font.render(str(times[x]), True, value_color[7])
+        screen.blit(lesson_t, (screen_width / 8, screen_height / 8 + lesson_t.get_height()*x))
+
+    times_limit = 10
+    sign_items = list(sign_bank.items())[::]
+
+    for s in sign_items:
+        if s[0] == 'steno':
+            sign_items.pop(sign_items.index(s))
+
+    sign_items = sorted(sign_items, key=lambda x: x[1][0], reverse=True)
+    for x in range(len(sign_items)):
+        if x > times_limit:
+            break
+        lesson_t = small_font.render(str((sign_items[x][1][0], sign_items[x][0])), True, value_color[7])
+        screen.blit(lesson_t, (screen_width / 2 + screen_width/4, screen_height / 8 + lesson_t.get_height()*x))
 
     walk = 0
     for x in range(len(phrase)):
@@ -512,12 +707,15 @@ while running:
         if x == phrase_pos:
             color = value_color[4]
         lesson_t = lable_font.render(str(phrase[x]), True, color)
-        screen.blit(lesson_t, (screen_width / 2 + walk, screen_height / 8))
+        screen.blit(lesson_t, (screen_width / 2 + walk - len(phrase)*8, screen_height / 4 + lesson_t.get_height()))
         walk += lesson_t.get_width()
 
     letter = digibetu[hand_value]
 
     hands[1] = letter
+
+
+
 
     ####code####
     if hands[0] == hands[1]:
@@ -527,7 +725,11 @@ while running:
             code += hands[0]
             code_0 = hands[0]
 
+            stenograph.append((code_0, round(time.time() - tts_1, 3), datetime.now()))
 
+            sign_bank['steno'] = stenograph
+
+            tts_1 = time.time()
 
             rv += digibet[code_0]
             rv = rv%bbv
@@ -541,19 +743,32 @@ while running:
             rule = np.array(rule)
 
 
+    ###typing###
     if phrase != '':
+
         if phrase != phrase_past:
+            tts[0] = time.time()
             score = 1
 
             if phrase not in sign_bank:
-                sign_bank[phrase] = (score, rv)
+                sign_bank[phrase] = (score, rv, [])
+
+                score, rv, times = sign_bank[phrase]
             else:
 
                 try:
-                    score, rv = sign_bank[phrase]
+                    score, rv , times = sign_bank[phrase]
                 except:
-                    score = sign_bank[phrase]
-                    rv = rv
+
+                    try:
+                        score, rv = sign_bank[phrase]
+                        times = []
+
+                    except:
+
+                        score = sign_bank[phrase]
+                        rv = rv
+                        times = []
 
 
 
@@ -567,17 +782,48 @@ while running:
             pickle.dump(sign_bank, outfile)
             outfile.close
 
-        elif letter == phrase[phrase_pos]:
+        elif letter == phrase[phrase_pos] or letter == phrase[phrase_pos:phrase_pos+2]:
+
+
+
             phrase_pos += 1
+            if len(letter) == 2:
+                phrase_pos += 1
+
+            if phrase_pos == 1:
+                tts[0] = time.time()
 
             if phrase_pos == len(phrase):
 
                 score += 1
                 phrase_pos = 0
+                tts[1] = time.time()
+
+
+                tts_0 = round(tts[1] - tts[0], 3)
+                tts[0] = time.time()
+
+                times_0 = []
+                t_max = 99999999999999999999999999999999999999
+                for t in times:
+                    if t < t_max and t > 0:
+                        times_0.append(round(t, 3))
+                times = times_0
+
+                # print()
+                # print(tts)
+                # print(tts_0)
+
+                times.append(tts_0)
+                times = sorted(times)
+
+                # print()
+                # print("times")
+                # print(times)
 
                 code = ''
 
-                sign_bank[phrase] = (score, rv)
+                sign_bank[phrase] = (score, rv, times)
 
                 filename = 'sign_bank/' + signame
                 outfile = open(filename, 'wb')
@@ -604,6 +850,115 @@ while running:
 
     lesson_t = lable_font.render(str(score), True, value_color[7])
     screen.blit(lesson_t, (screen_width / 2, screen_height / 8 - 64))
+
+
+
+    ###synth###
+
+
+
+    beat = 1
+    #####drums####
+
+    if time.time() - time_d > beat/8:
+
+        # print(tempo)
+
+
+        if tempo % 16 == 0:
+            # print('kick')
+            kick_sound.play()
+
+        if tempo % 16 == 10:
+            # print('kick')
+            kick_sound.play()
+
+
+
+        if tempo%16 == 4:
+            snare_sound.play()
+
+        if tempo%16 == 12:
+            hihat_sound.play()
+
+
+        tempo += 1
+
+        time_d = time.time()
+
+
+
+    volume = 0.5
+
+    ######bong#####
+    if time.time() - time_b > beat:
+
+
+        flatten = water.flatten()
+        scaled = (flatten * 2 - 1) * max_amplitude
+        sample = scaled.astype(np.int16)
+        stereo = sample.reshape(-1, 1)
+        stack = np.column_stack((stereo, stereo))
+        flow_sound = pygame.sndarray.make_sound(stack)
+
+        flow_sound.play()
+
+
+
+        # print()
+        # print('bong')
+        #
+
+
+
+        # note_value = digibet[code_0]
+        #
+        #
+        # # print(note_value)
+        #
+        # octave = 24
+
+        # note_shape = int(note_value/8)
+        # note_scale = sum(waffle_house[:note_value%8])
+        #
+        # note_scale = sum(scale[:note_value%8])
+        # note_scale += track_shift
+        # note_scale = note_scale + octave % len(key_sin)
+
+        # print(note_shape)
+        # print(note_scale)
+
+        # note_shape = 0
+        # note_scale = note_value + octave
+        #
+        # if note_shape == 0:
+        #
+        #     key_sin[note_scale].set_volume(volume)
+        #     key_sin[note_scale].play()
+        #
+        # elif note_shape == 1:
+        #
+        #     key_square[note_scale].set_volume(volume-.25)
+        #     key_square[note_scale].play()
+        #
+        # elif note_shape == 2:
+        #
+        #     key_saw[note_scale].set_volume(volume-.25)
+        #     key_saw[note_scale].play()
+        #
+        # elif note_shape == 3:
+        #
+        #     key_triangle[note_scale].set_volume(volume)
+        #     key_triangle[note_scale].play()
+        #
+
+
+        time_b = time.time()
+
+
+
+
+
 
 
     ####events####
@@ -952,8 +1307,12 @@ while running:
 
             elif event.key == pygame.K_BACKSPACE:
                 phrase = phrase[:-1]
+                phrase_pos -= 1
+                if phrase_pos < 0:
+                    phrase_pos = 0
 
     #####################
+    pygame.display.flip()
     pygame.display.flip()
 
 camera.stop()
